d9dfcf597b95b1ef72215ba7423590f75fe05a50
Rafnix Guzman <rafnixg@gmail.com>
2020-04-02 18:04:10 -0500

    [IMP] Normalizacion de la data exportada data.csv

--- etl.py
+++ etl.py
@@ -4,6 +4,9 @@ import io
 import requests
 import os
 from datetime import date, timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 def etl():
@@ -26,12 +29,22 @@ def etl():
         print(file)
         url = r"https://raw.githubusercontent.com/DataScienceResearchPeru/covid-19_latinoamerica/master/latam_covid_19_data/latam_covid_19_daily_reports/{date}.csv".format(date=file)
         raw_string = requests.get(url).content
-        df = pd.read_csv(io.StringIO(raw_string.decode('utf-8')))
         if b'404: Not Found\n' not in raw_string:
-            df.to_csv('data/{}.csv'.format(file), index=False)
-        df.rename(columns={'Last Update': 'Date'}, inplace=True)
-        df['Date'] = pd.to_datetime(file)
-        files.append(df)
+            try:
+                df = pd.read_csv(io.StringIO(raw_string.decode('utf-8')))
+                df.to_csv('data/{}.csv'.format(file), index=False)
+                df.rename(columns={'Last Update': 'Date'}, inplace=True)
+                df['Date'] = pd.to_datetime(file)
+                df = df.fillna(0).replace({'missing': 0})
+                df['Confirmed'] = df['Confirmed'].astype(int)
+                df['Deaths'] = df['Deaths'].astype(int)
+                df['Recovered'] = pd.to_numeric(df['Recovered']).astype(int)
+                files.append(df)
+            except:
+                _logger.warning(f'Error in file format {file}.csv')
+        else:
+            _logger.warning(f'File {file} Not Found')
+
     df = pd.concat(files, axis=0, ignore_index=True, sort=False)
 
     return df
